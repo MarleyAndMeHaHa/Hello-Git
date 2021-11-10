5f181077b6b0a8494e1b15a8e578b59544335da6
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-10 10:36:46 +0000

    Rainbow List

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 console.log("Hello World");
 
-console.log("Its a beautiful day outside, birds are singing, flowers are blooming")
+console.log("Its a beautiful day outside, birds are singing, flowers are blooming, On days like this kids like you, SHOULD BE BURNING IN HELL")
 
 
 function sayHello(name){
-   return `Hello${name}`;
+   return `Hello ${name}`;
 
 
 }
 console.log(sayHello('Marley O '))
+
+const Rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+console.log(Rainbow)
