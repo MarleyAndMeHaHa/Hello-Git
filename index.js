856414259f3c3b0370a25dc331c60b802ba64214
Year 12 <year12@mar-admins-MacBook-Air-3.local>
2021-11-10 10:19:56 +0000

    My Name

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 console.log("Hello World");
 
 console.log("Its a beautiful day outside, birds are singing, flowers are blooming")
+
+
+function sayHello(name){
+   return `Hello${name}`;
+
+
+}
+console.log(sayHello('Marley O '))
